90d276f39a08ff19bedc0b7384e26a66f5b8c87f
Alexandr Spesivtsev <dart_sas@mail.ru>
2016-06-01 09:18:15 +0700

    translate failed login message to russian

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Неправильный логин или пароль..',
+    'throttle' => 'Слишком много попыток ввода. Пожалуйста, повторите через :seconds секунд.',
 
 ];
