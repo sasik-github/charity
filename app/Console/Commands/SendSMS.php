9a5753eeef71936cbfb6dad0abbf1660160ac1b4
Alexandr Spesivtsev <dart_sas@mail.ru>
2016-07-22 10:40:55 +0700

    add smsc command, update smsc package

---
+++ app/Console/Commands/SendSMS.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Sasik\SmscGateway\SMSGateway;
+
+class SendSMS extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sms:send {telephone} {message}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send sms to a telephone number';
+
+    /**
+     * @var SMSGateway
+     */
+    private $SMSGateway;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param SMSGateway $SMSGateway $SMSGateway
+     */
+    public function __construct(SMSGateway $SMSGateway)
+    {
+        parent::__construct();
+
+        $this->SMSGateway = $SMSGateway;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $telephone = $this->argument('telephone');
+        $message = $this->argument('message');
+
+        $this->SMSGateway->send($telephone, $message);
+    }
+}
